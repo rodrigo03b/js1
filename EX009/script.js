ac9a229d30649df63238a31176ca8010417e2838
rodrigo03b <145490620+rodrigo03b@users.noreply.github.com>
2023-11-08 15:28:17 -0300

    att

--- EX009/script.js
+++ EX009/script.js
@@ -3,18 +3,21 @@ function carregar(){
     var img = window.document.getElementById('Imagens')
     var dia = new Date()
     var hora = dia.getHours()
-    msg.innerHTML = `ágora são ${hora} rs`
+    msg.innerHTML = `agora são ${hora} rs`
     if(hora >= 0 && hora < 12){
         //bom dia
         img.src = 'dia.png'
+        msg.innerHTML += '<P>bom dia</p>'
         document.body.style.background = '#e2cd9f'
     }else if(hora >= 12 && hora <= 18){
         //boa tarde
         img.src = 'tarde.png'
+        msg.innerHTML += '<P>boa tarde</p>'
         document.body.style.background = '#b9846f'
     }else{
         //boa noite
         img.src = 'noite.png'
         document.body.style.background = '#515'
+        msg.innerHTML += '<P>boa noite</p>'
     }
     }
---
+++ ex011/script.js
@@ -0,0 +1,10 @@
+function clicar(){
+    var cor1 = document.getElementById('caixa2')
+    cor1.style.backgroundColor = 'red'
+}
+function parte1(){
+    var clica = document.getElementById('txt2')
+    var resultado = document.getElementById('area1')
+    resultado.innerHTML += '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aliquam dolorum fuga, veniam cum esse sint itaque culpa aut, deserunt cupiditate dolore quidem suscipit? Porro ducimus vero magni odio quasi?</p>'
+}
+
