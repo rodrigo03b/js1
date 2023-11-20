3ffa6fb761bcd33e021591bf122191575a28a49c
rodrigo03b <145490620+rodrigo03b@users.noreply.github.com>
2023-11-20 20:38:50 -0300

    att

--- ex022/node_182617c23bdf5.tmp.js
+++
@@ -1,9 +0,0 @@
-var c = 1
-while(c < 6){
-    console.log('tudo bem')
-}
-/*console.log('tudo bem')
-console.log('tudo bem')
-console.log('tudo bem')
-console.log('tudo bem')
-console.log('tudo bem')*/
