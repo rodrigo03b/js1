32f299123660a3788247601b50e79d16bb70778c
rodrigo03b <145490620+rodrigo03b@users.noreply.github.com>
2023-11-20 16:48:37 -0300

    att

---
+++ ex022/node_182617c23bdf5.tmp.js
@@ -0,0 +1,9 @@
+var c = 1
+while(c < 6){
+    console.log('tudo bem')
+}
+/*console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')*/
---
+++ ex022/script.js
@@ -0,0 +1,8 @@
+var c = 1
+while(c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}
+//emquanto
+/*estrutura de repetição com teste logico no inicio*/
+/*testa,executa e faz loop*/
